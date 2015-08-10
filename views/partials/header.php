271eded7ae4d1d4cb46935a0ae1a68aeb732d25e
Timothy <tbirrell@mastersolutions.biz>
2015-08-10 16:00:42 -0500

    working template

--- views/partials/header.php
+++ views/partials/header.php
@@ -7,17 +7,19 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">Adnormal</a>
+			<a class="navbar-brand" href="#">Project name</a>
 		</div>
 		<div id="navbar" class="navbar-collapse collapse">
-			<ul class="nav navbar-nav navbar-right">
-				<li><a href="#">Home</a></li>
-				<li><a href="#">Add ad</a></li>
-				<li><a href="#">Logout</a></li>
-			</ul>
 			<form class="navbar-form navbar-right">
-				<input type="text" class="form-control" placeholder="Search...">
+				<input type="text" class="form-control" placeholder="Username">
+				<input type="password" class="form-control" placeholder="Password">
 			</form>
+			<ul class="nav navbar-nav navbar-right">
+			<li><a href="#">Dashboard</a></li>
+			<li><a href="#">Settings</a></li>
+			<li><a href="#">Profile</a></li>
+			<li><a href="#">Help</a></li>
+			</ul>
 		</div>
 	</div>
 </nav>
--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,23 +1,13 @@
 <div class="col-sm-3 col-md-2 sidebar">
 	<form>
 		<div class="form-group">
-			<input type="text" id="search" class="form_control" placeholder="Search...">
+			<input type="text" id="search" class="form-control" placeholder="Hit Enter To Search...">
 		</div>
-			<button class="btn btn-primary btn-xs">Search</button>
 	</form>
-	</ul>
-	<h4>header</h4>
-	<ul class="nav nav-sidebar">
-		<li><a href="">Nav item</a></li>
-		<li><a href="">Nav item again</a></li>
-		<li><a href="">One more nav</a></li>
-		<li><a href="">Another nav item</a></li>
-		<li><a href="">More navigation</a></li>
-	</ul>
-	<h4>Hi Mom!</h4>
-	<ul class="nav nav-sidebar">
-		<li><a href="">Nav item again</a></li>
-		<li><a href="">One more nav</a></li>
-		<li><a href="">Another nav item</a></li>
-	</ul>
+
+	
+	<div class="form-group">
+		<button class="btn btn-primary">Advanced Search</button>
+	</div>
+	<button class="btn btn-primary">FAQ</button>
 </div>
