6fdb72a3c3cca6da58b77127e7a098c0e266ba15
Tarek S Hafez <thafez@ymail.com>
2015-08-10 16:03:27 -0500

    Made image_url not required field.

--- database/migration.php
+++ database/migration.php
@@ -8,7 +8,7 @@
         id INT UNSIGNED AUTO_INCREMENT,
         title CHAR(255) NOT NULL,
         price DECIMAL(10, 2) NOT NULL,
-        image_url CHAR(255) NOT NULL,
+        image_url CHAR(255),
         location CHAR(255) NOT NULL,
         sell_by DATE NOT NULL,
         categories CHAR(255) NOT NULL,
