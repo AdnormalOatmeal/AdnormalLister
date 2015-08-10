6e59536151a168bf61a82f9a236a395a40003b9a
Timothy <tbirrell@mastersolutions.biz>
2015-08-10 15:25:31 -0500

    set up page template

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,1 +1,23 @@
-navbar.php
+<div class="col-sm-3 col-md-2 sidebar">
+	<form>
+		<div class="form-group">
+			<input type="text" id="search" class="form_control" placeholder="Search...">
+		</div>
+			<button class="btn btn-primary btn-xs">Search</button>
+	</form>
+	</ul>
+	<h4>header</h4>
+	<ul class="nav nav-sidebar">
+		<li><a href="">Nav item</a></li>
+		<li><a href="">Nav item again</a></li>
+		<li><a href="">One more nav</a></li>
+		<li><a href="">Another nav item</a></li>
+		<li><a href="">More navigation</a></li>
+	</ul>
+	<h4>Hi Mom!</h4>
+	<ul class="nav nav-sidebar">
+		<li><a href="">Nav item again</a></li>
+		<li><a href="">One more nav</a></li>
+		<li><a href="">Another nav item</a></li>
+	</ul>
+</div>
