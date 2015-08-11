3aedb4dd0a56a0be451c154aad14c584d62c3889
Timothy <tbirrell@mastersolutions.biz>
2015-08-11 14:10:06 -0500

    added modals

--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,35 +1,108 @@
-<div class="navmenu navmenu-default navmenu-fixed-left offcanvas-sm">
-<a class="navmenu-brand visible-md visible-lg" href="#">Project name</a>
-<ul class="nav navmenu-nav">
-	<li class="active"><a href="./">Slide in</a></li>
-	<li><a href="../navmenu-push/">Push</a></li>
-	<li><a href="../navmenu-reveal/">Reveal</a></li>
-	<li><a href="../navbar-offcanvas/">Off canvas navbar</a></li>
-</ul>
-<ul class="nav navmenu-nav">
-	<li><a href="#">Link</a></li>
-	<li><a href="#">Link</a></li>
-	<li><a href="#">Link</a></li>
-	<li class="dropdown">
-		<a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <b class="caret"></b></a>
-		<ul class="dropdown-menu navmenu-nav">
-			<li><a href="#">Action</a></li>
-			<li><a href="#">Another action</a></li>
-			<li><a href="#">Something else here</a></li>
-			<li class="divider"></li>
-			<li class="dropdown-header">Nav header</li>
-			<li><a href="#">Separated link</a></li>
-			<li><a href="#">One more separated link</a></li>
-		</ul>
-	</li>
-</ul>
+<style>
+
+.search-form {
+	margin-top: 10px;
+	border-bottom: 1px solid #C6C6C6;
+	padding-bottom: 10px;
+}
+.links {
+	position: absolute;
+	bottom: 10px;
+	text-align: center;
+	width: 90%;
+}
+.login {
+	margin: 10px 0px;
+}
+
+</style>
+
+<!-- MODALS -->
+<div class="modal fade" id="faqModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">FAQ</h4>
+			</div>
+			<div class="modal-body">
+			<!-- BEGINNING OF MODAL BODY. DO NOT PUT CUSTOM CODE BEFORE HERE -->
+			 FAQ
+			<!-- END OF PAGE MODAL. DO NOT PUT CUSTOM CODE AFTER HERE -->
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="aboutModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">About</h4>
+			</div>
+			<div class="modal-body">
+			<!-- BEGINNING OF MODAL BODY. DO NOT PUT CUSTOM CODE BEFORE HERE -->
+			About
+			<!-- END OF PAGE MODAL. DO NOT PUT CUSTOM CODE AFTER HERE -->
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="modal fade" id="contactModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title" id="myModalLabel">About</h4>
+			</div>
+			<div class="modal-body">
+			<!-- BEGINNING OF MODAL BODY. DO NOT PUT CUSTOM CODE BEFORE HERE -->
+			About
+			<!-- END OF PAGE MODAL. DO NOT PUT CUSTOM CODE AFTER HERE -->
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
 </div>
 
+<!-- NAVBAR -->
+<div class="navmenu navmenu-default navmenu-fixed-left offcanvas-sm container">
+	<a class="navmenu-brand visible-md visible-lg" href="http://adnormallister.dev">Adnormal Oatmeal</a>
+	<form class="form-inline search-form">
+		<div class="form-group">
+			<input type="text" class="form-control" id="search" placeholder="Search..." style="width: 225px;">
+		</div>
+		<div class="form-group">
+			<button class="btn btn-default"><span class="glyphicon glyphicon-search"></span></button>
+		</div>
+	</form>
+
+	<div class="login">
+		<button class="btn btn-default">Login</button>
+	</div>
+
+
+	<div class="links">
+		<a href="#" data-toggle="modal" data-target="#faqModal">FAQ</a>
+		 - 
+		<a href="#" data-toggle="modal" data-target="#aboutModal">About</a>
+		 - 
+		<a href="#" data-toggle="modal" data-target="#contactModal">Contact</a>
+	</div>
+</div>
 <div class="navbar navbar-default navbar-fixed-top hidden-md hidden-lg">
-<button type="button" class="navbar-toggle" data-toggle="offcanvas" data-target=".navmenu">
-	<span class="icon-bar"></span>
-	<span class="icon-bar"></span>
-	<span class="icon-bar"></span>
-</button>
-<a class="navbar-brand" href="#">Project name</a>
+	<button type="button" class="navbar-toggle" data-toggle="offcanvas" data-target=".navmenu">
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+		<span class="icon-bar"></span>
+	</button>
+	<a class="navbar-brand" href="#">Adnormal Oatmeal</a>
 </div>
