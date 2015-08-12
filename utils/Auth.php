a11638217558524245181f0cd39ffeff7ad98f1a
Tarek S Hafez <thafez@ymail.com>
2015-08-12 11:32:28 -0500

    formatted utils/Auth.php

--- utils/Auth.php
+++ utils/Auth.php
@@ -7,34 +7,38 @@ require_once '../bootstrap.php';
 class Auth {
 
 		public static function attempt($password, $username, $dbc) {
-		$select = "SELECT id FROM users WHERE user_name = :username AND password = :password";
-		$stmt = $dbc->prepare($select);
-		$stmt->bindValue(':username', $username, PDO::PARAM_STR);
-		$stmt->bindValue(':password', password_hash($password, PASSWORD_DEFAULT), PDO::PARAM_STR);
+			$select = "SELECT id FROM users 
+						WHERE user_name = :username
+						AND password = :password";
 
-		$_SESSION["id"] = $stmt->fetchAll(PDO::FETCH_ASSOC);
+			$stmt = $dbc->prepare($select);
+
+			$stmt->bindValue(':username', $username, PDO::PARAM_STR);
+			$stmt->bindValue(':password', password_hash($password, PASSWORD_DEFAULT), PDO::PARAM_STR);
+			$stmt->execute();
+
+			$_SESSION["id"] = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 
-		if (isset($_SESSION["id"])) {
-			return true;
-		} else {
-			return false;
+			return static::check();
 		}
-	}
-	public static function check() {
-		if (isset($_SESSION["id"])) {
-			return true;
-		} else {
-			return false;
+
+		public static function check() {
+			if (isset($_SESSION["id"])) {
+				return true;
+			} else {
+				return false;
+			}
 		}
-	}
-	public static function user() {
-		if (self::check()) {
-			return $_SESSION["id"];
+
+		public static function user() {
+			if (self::check()) {
+				return $_SESSION["id"];
+			}
 		}
-	}
-	public static function logout() {
-		$_SESSION = array();
-		session_destroy();
-	}
+
+		public static function logout() {
+			$_SESSION = array();
+			session_destroy();
+		}
 }
